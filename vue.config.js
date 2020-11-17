c84474530ce162a1e51928bb2b94ab456f3fbc5e
Adam Brown <adam.brown@tapiit.com>
2020-11-17 00:13:41 +0000

    fixed vue.config path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/gift-tracker/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/gift-list/" : "/"
 }
